efc68ea26c7e027ec1894b41a5cfc8337edbe5a4
Robert Hodgen <roberthodgen@gmail.com>
2015-01-31 16:13:15 -0500

    Copyright notice

--- src/model.py
+++ src/model.py
@@ -1,8 +1,10 @@
 """
+Copyright (c) 2015 Robert Hodgen
 
-mode.py holds out data models
-
+All rights reserved.
 """
+
+
 from google.appengine.ext import ndb
 
 from datetime import datetime, timedelta, tzinfo
--- src/utilities.py
+++ src/utilities.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) 2015 Robert Hodgen
+
+All rights reserved.
+"""
+
+
 import string
 
 import re

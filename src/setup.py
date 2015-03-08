1b170afebf6ce066ecb0337f1ae5023a0c1d23a8
Robert Hodgen <roberthodgen@gmail.com>
2015-03-07 23:04:31 -0800

    Changed default Labels

--- src/setup.py
+++ src/setup.py
@@ -13,5 +13,6 @@ def default_project_labels(project):
     enahncement = model.Label.create_label('Enhancement',
         '#4a89dc', project.key)
     bug = model.Label.create_label('Bugs', '#da4453', project.key)
-    analyze = model.Label.create_label('Analyze', '#967acd', project.key)
+    idea = model.Label.create_label('Idea', '#967acd', project.key)
+    question = model.Label.create_label('Question', '#ffce54', project.key)
     feature = model.Label.create_label('Feature', '#37bc9b', project.key)
